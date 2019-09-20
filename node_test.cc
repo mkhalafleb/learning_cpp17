64445eb934e97f69dac64124dfcf77b15bf312fb
Mario Khalaf <mkhalaf@google.com>
2019-09-20 19:07:32 -0400

    Added skeleton node_test

---
+++ node_test.cc
@@ -0,0 +1,18 @@
+#include "node.h"
+#include "gtest/gtest.h"
+
+namespace {
+
+// The fixture for testing class Node
+class NodeTest : public ::testing::Test {
+ protected:
+};
+
+TEST_F(NodeTest, TwoNodes) {
+  graphnode::Node n(0);
+}
+
+
+
+}  // namespace
+
