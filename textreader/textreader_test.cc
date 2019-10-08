feb0a8ae11b9c24c671e43d8ad4cffb02c020cad
Mario Khalaf <mkhalaf@google.com>
2019-10-08 19:15:15 -0400

    added std::filesystem

--- textreader/textreader_test.cc
+++ textreader/textreader_test.cc
@@ -1,21 +1,29 @@
 #include "textreader.h"
 #include "gtest/gtest.h"
 #include <string>
-#include <iostream>
+#include <cstdlib>
+#include <filesystem>
 
 namespace {
 
 // The fixture for testing class TextReader 
 class TextReaderTest : public ::testing::Test {
+ public:
+  static std::string FullPath(const std::string &endpath);
  protected:
+};
+
+std::string TextReaderTest::FullPath(const std::string &endpath) {
+  std::filesystem::path basepath = std::getenv("TEST_SRCDIR");
+  basepath /= std::filesystem::path(std::getenv("TEST_WORKSPACE"));
+  basepath /= std::filesystem::path("textreader");
+  basepath /= std::filesystem::path(endpath);
+  return(basepath.string());
 };
 
 // Tests that the Foo::Bar() method does Abc.
 TEST_F(TextReaderTest, CREATENAME) {
-  std::string test_src = getenv("TEST_SRCDIR");
-  std::string test_workspace = getenv("TEST_WORKSPACE");
-  test_src += "/" + test_workspace + "/textreader/Hello";
-  textreader::TextReader txtreader(test_src);
+  textreader::TextReader txtreader(TextReaderTest::FullPath("Hello"));
   EXPECT_TRUE(txtreader.CreateAdjacency());
   EXPECT_TRUE(txtreader.VerifyAdjacency());
  }
