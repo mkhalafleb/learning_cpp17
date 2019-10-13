1a32b17564be85ce29f5e16dd18e13b1419e57e1
Mario Khalaf <mkhalaf@google.com>
2019-10-13 18:13:35 -0400

    Added 2 new directories

---
+++ nodeid/nodeid.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include "nodeid.h"
+
+
+namespace nodeid {
+
+NodeId::NodeId(int id): id_(id) {};
+
+
+bool NodeId::operator==(const NodeId &nodeid) const {
+  bool bothvalid = IsValid() && nodeid.IsValid();
+  return (bothvalid && (id_ ==  nodeid.id_));
+}
+
+bool NodeId::operator!=(const NodeId &nodeid) const {
+  return(!((*this) == nodeid));
+}
+
+std::ostream& operator<<(std::ostream &os, const NodeId &nodeid) {
+  os << nodeid.id_;
+  return(os);
+}
+
+// A nodeID is valid only if it is assigned a positive number or 0
+bool NodeId::IsValid() const {
+  // Need to handle Overflow somehow.
+  return (id_ >= 0);
+};
+
+} // namespace nodeid
+
+
+
+
+
+
+
