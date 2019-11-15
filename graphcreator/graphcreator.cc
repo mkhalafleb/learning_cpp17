47feedb6cf5fd25dadeecafd4752c79c84578292
Mario Khalaf <mkhalaf@google.com>
2019-11-15 14:45:07 -0500

    Added more processing to graphcreator

--- graphcreator/graphcreator.cc
+++ graphcreator/graphcreator.cc
@@ -1,5 +1,7 @@
 #include "graphcreator.h"
 #include "graph.h"
+#include "textreader/textreader.h"
+#include "fixadjacency/fixadjacency.h"
 #include <string>
 #include <optional>
 
@@ -9,12 +11,39 @@ namespace graphcreator {
 GraphCreator::GraphCreator(const std::string &filename) : filename_(filename) {};
 
 
+bool GraphCreator::GetEdgeList() {
+
+  textreader::TextReader textparser(filename_);
+  if (!textparser.CreateAdjacency()) {
+    return(false);
+  }
+  auto adjvec = textparser.GetAdjacency();
+
+  fixadjacency::FixAdjacency adjfix(adjvec);
+
+  if (!adjfix.VerifyAdjacency()) {
+    return(false);
+  }
+
+  if (!adjfix.RepairAdjacency()) {
+    return(false);
+  }
+
+  adjlist_ = adjfix.GetAdjacency();
+  return(true);
+
+}
 
 // Create Graph from the file that is passed on to the class and saved in
 // filename_
 
 // Return nullopt if the graph can't be created
 std::optional<graph::Graph> GraphCreator::CreateGraph() {
+
+  // Read the file 
+  if (!GetEdgeList()) {
+    return(std::nullopt);
+  }
   return(std::nullopt);
 }
 
--- graphcreator/graphcreator.h
+++ graphcreator/graphcreator.h
@@ -3,6 +3,8 @@
 #include "graph.h"
 #include <string>
 #include <optional>
+#include <utility>
+#include <list>
 
 namespace graphcreator {
 
@@ -11,12 +13,16 @@ class GraphCreator {
   // This is an absolute path for a file
   GraphCreator(const std::string &filename);
 
+  // Fill the adjlist_ by reading the filename_
+  bool GetEdgeList();
+
   // We are using optional here in a way that if it is not returned
   // then we failed in GraphCreation
   std::optional<graph::Graph> CreateGraph();
 
  private:
   std::string filename_;
+  std::list<std::pair<unsigned int, std::optional<unsigned int>>> adjlist_;
 };
 
 } // namespace graphcreator
