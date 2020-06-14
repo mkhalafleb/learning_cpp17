e3bf8f2b17e3d5d4c42dc30e5b05c9b84bac77c7
Mario Khalaf <mkhalaf@google.com>
2020-06-14 23:19:23 +0000

    Added more files to the refactored library

---
+++ graph/graph.h
@@ -0,0 +1,35 @@
+#ifndef MKHALAF_LEARNINGCPP17_GRAPH
+#define MKHALAF_LEARNINGCPP17_GRAPH
+#include <list>
+#include <memory>
+#include <vector>
+#include <optional>
+#include <utility>
+#include "node/node.h"
+
+namespace graph {
+
+class Graph {
+ public:
+  // The owner of the node is the graph so everyone else deals with weak ptrs
+  // to the node so if I create a node, I get a weak ptr back of the node
+  std::weak_ptr<graphnode::Node> AddNode(unsigned int node_id);
+
+  // This graph currently only supports one edge out of each node maximum
+  std::vector<std::pair<unsigned int, std::optional<unsigned int>>> GetAdjList() const;
+
+ private:
+
+  void FillAdjPair(const graphnode::Node &node,
+       std::vector<std::pair<unsigned int, std::optional<unsigned int>>> &adjlist) const;
+  static unsigned int lastnodeid_;
+  std::list<std::shared_ptr<graphnode::Node>> nodelist_;
+};
+
+} // namespace graph
+
+
+#endif  // MKHALAF_LEARNINGCPP17_GRAPH
+
+
+
