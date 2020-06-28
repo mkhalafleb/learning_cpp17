8a1c8aa1333f7d69c0810bc7a931fcd3dac9baa3
Mario Khalaf <mkhalaf@google.com>
2020-06-28 04:50:41 +0000

    Added clang-format to files

--- graph/graph.h
+++ graph/graph.h
@@ -1,39 +1,37 @@
 #ifndef MKHALAF_LEARNINGCPP17_GRAPH
 #define MKHALAF_LEARNINGCPP17_GRAPH
+#include "node/node.h"
 #include <list>
 #include <memory>
-#include <vector>
 #include <optional>
 #include <utility>
-#include "node/node.h"
+#include <vector>
 
 namespace graph {
 
 class Graph {
- public:
+public:
   // The owner of the node is the graph so everyone else deals with weak ptrs
   // to the node so if I create a node, I get a weak ptr back of the node
   std::weak_ptr<graphnode::Node> AddNode(const nodeid::NodeId &node_id);
 
   // This graph currently only supports one edge out of each node maximum
   // This is only for debugging
-  std::vector<std::pair<unsigned int, std::optional<unsigned int>>> GetAdjList() const;
+  std::vector<std::pair<unsigned int, std::optional<unsigned int>>>
+  GetAdjList() const;
 
   // Get a list of weak pointer to all the graph nodes
-  const std::list<std::weak_ptr<graphnode::Node>> GetNodeList() const; 
+  const std::list<std::weak_ptr<graphnode::Node>> GetNodeList() const;
 
- private:
-
-  void FillAdjPair(const graphnode::Node &node,
-       std::vector<std::pair<unsigned int, std::optional<unsigned int>>> &adjlist) const;
+private:
+  void
+  FillAdjPair(const graphnode::Node &node,
+              std::vector<std::pair<unsigned int, std::optional<unsigned int>>>
+                  &adjlist) const;
   static unsigned int lastnodeid_;
   std::list<std::shared_ptr<graphnode::Node>> nodelist_;
 };
 
 } // namespace graph
 
-
-#endif  // MKHALAF_LEARNINGCPP17_GRAPH
-
-
-
+#endif // MKHALAF_LEARNINGCPP17_GRAPH
