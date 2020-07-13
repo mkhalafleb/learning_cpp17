fa1052b5c936d021e9ad1f2219ebf15ad1f59a57
Mario Khalaf <mkhalaf@google.com>
2020-07-13 02:02:08 +0000

    Fixed DFS and added BFS

--- algorithm/algorithm.h
+++ algorithm/algorithm.h
@@ -4,6 +4,7 @@
 #include "node/node.h"
 #include "nodeid/nodeid.h"
 #include <memory>
+#include <queue>
 
 namespace algorithm {
 
@@ -11,9 +12,14 @@ namespace algorithm {
 // with a debug ID for each node traversed
 std::string GraphDFS(const graph::Graph &bgraph);
 
+// Now BFS
+std::string GraphBFS(const graph::Graph &bgraph);
+
 void GraphNodeClear(const graph::Graph &bgraph);
 
 std::string GraphDFSVisit(std::weak_ptr<graphnode::Node> node_ptr);
+
+std::string GraphBFSVisit(std::queue<std::weak_ptr<graphnode::Node>> &node_ptr);
 
 } // namespace algorithm
 
